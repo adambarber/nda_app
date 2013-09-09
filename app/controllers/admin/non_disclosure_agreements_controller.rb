ead8c89c4a809495014c0fc652e4d4c0a5f1bc70
Adam Barber <adam@adambarber.tv>
2013-09-08 19:04:02 -0700

    add edit action

--- app/controllers/admin/non_disclosure_agreements_controller.rb
+++ app/controllers/admin/non_disclosure_agreements_controller.rb
@@ -6,4 +6,23 @@ class Admin::NonDisclosureAgreementsController < AdminController
   def show
     @nda = NonDisclosureAgreement.find(params[:id])
   end
+
+  def edit
+    @nda = NonDisclosureAgreement.find(params[:id])
+  end
+
+  def update
+    @nda = NonDisclosureAgreement.find(params[:id])
+    if @nda.update_attributes(nda_params)
+      redirect_to admin_non_disclosure_agreement_path(@nda), notice: "Success!"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def nda_params
+    params.require(:non_disclosure_agreement).permit(:name, :body)
+  end
 end
--- spec/controllers/admin/non_disclosure_agreements_controller_spec.rb
+++ spec/controllers/admin/non_disclosure_agreements_controller_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
 describe Admin::NonDisclosureAgreementsController do
+  let(:non_disclosure_agreement) { stub_model(NonDisclosureAgreement) }
+  before(:each) do
+    @nda = NonDisclosureAgreement.create(name: "First NDA", body: "This is the text of an NDA")
+  end
+
   describe "GET 'index'" do
-
-    let(:non_disclosure_agreement) { stub_model(NonDisclosureAgreement) }
-    before(:each) do
-      @nda = NonDisclosureAgreement.create(name: "First NDA", body: "This is the text of an NDA")
-    end
-
     describe 'response' do
       it "returns http success" do
         get 'index'
@@ -36,24 +35,51 @@ describe Admin::NonDisclosureAgreementsController do
   end
 
   describe "Get 'show'" do
-
-    let(:non_disclosure_agreement) { stub_model(NonDisclosureAgreement) }
-    before(:each) do
-      @nda = NonDisclosureAgreement.create(name: "First NDA", body: "This is the text of an NDA")
-    end
-
     describe 'response' do
       it 'returns http success' do
         get 'show', id: @nda
         expect(response).to be_success
       end
+      it "assigns @nda" do
+        get 'edit', id: @nda
+        expect(assigns(:nda)).to eq(@nda)
+      end
     end
-
     describe 'views' do
       render_views
       it 'should show NDA name' do
-        get 'show', id: @nda
+        get 'show', id: @nda.id
         expect(response.body).to have_content(@nda.name)
+      end
+    end
+  end
+
+  describe "Get 'edit'" do
+    describe 'requests' do
+      it "returns http success" do
+        get 'edit', id: @nda
+        response.should be_success
+      end
+      it "assigns @nda" do
+        get 'edit', id: @nda
+        expect(assigns(:nda)).to eq(@nda)
+      end
+    end
+    describe 'views' do
+      render_views
+      it 'shows edit form' do
+        get 'edit', id: @nda
+        expect(response.body).to have_selector('form')
+      end
+    end
+  end
+
+  describe "Put 'update" do
+    describe 'requests' do
+      it "should update the object's attributes" do
+        put :update, id: @nda.id, non_disclosure_agreement: { name: "new name", body: "new body text"}
+        @nda.reload
+        expect(@nda.name).to eq "new name"
       end
     end
   end
