a83bfdcee77b4e35a9b4ad2dfa75a4ad5831981e
Adam Barber <adam@adambarber.tv>
2013-09-11 19:06:10 -0700

    Added simplecov and tests for signatures controller

--- Gemfile
+++ Gemfile
@@ -37,4 +37,5 @@ group :test do
   gem 'database_cleaner', '1.0.1'
   gem 'email_spec'
   gem 'shoulda'
+  gem 'simplecov', require: false
 end
--- spec/controllers/signatures_controller_spec.rb
+++ spec/controllers/signatures_controller_spec.rb
@@ -3,11 +3,28 @@ require 'spec_helper'
 describe SignaturesController do
 
   describe "GET 'index'" do
+    describe 'requests' do |variable|
+      it "returns http success" do
+        @nda = FactoryGirl.create(:non_disclosure_agreement, {name: "Test", body: "Some Body"})
+        get 'index'
+        response.should be_success
+      end
+    end
 
-    it "returns http success" do
-      @nda = FactoryGirl.create(:non_disclosure_agreement, {name: "Test", body: "Some Body"})
-      get 'index'
-      response.should be_success
+    describe 'views' do
+      render_views
+      it 'renders signature submit button' do
+        @nda = FactoryGirl.create(:non_disclosure_agreement, {name: "Test", body: "Some Body"})
+        @signature = FactoryGirl.build(:signature, non_disclosure_agreement: @nda)
+        get 'index'
+        expect(response.body).to have_button "Submit Document Signature"
+      end
+
+      it 'shows the non disclosure agreement' do
+        @nda = FactoryGirl.create(:non_disclosure_agreement, {name: "My Awesome Non Disclosure Agreement", body: "Some Body"})
+        get 'index'
+        expect(response.body).to have_content "My Awesome Non Disclosure Agreement"
+      end
     end
   end
 
